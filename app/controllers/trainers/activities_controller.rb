db6dc0505ea8f04fead3945be84a08e3aa0f6e20
Marta Kask <marta.m.kask@gmail.com>
2021-03-11 22:06:39 +0100

    created trainer activities show, new, create

--- app/controllers/trainers/activities_controller.rb
+++ app/controllers/trainers/activities_controller.rb
@@ -1,2 +1,33 @@
 class Trainers::ActivitiesController < ApplicationController
+
+  def show
+    @activity = Activity.find(params[:id])
+  end
+
+  # def dashboard
+  # end
+
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.user = current_user
+    if @activity.save
+      redirect to trainers_activity_path(@activity), notice: "Class was successfully created"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def activity_params
+    params.require(:activity).permit(:category, :time, :date, :frequency, :equipment, :duration, :description)
+  end
+
 end
+
+
+
