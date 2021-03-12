c08a985ec3ee9f0cc57813e9c979ab92b912501f
Marta Kask <marta.m.kask@gmail.com>
2021-03-12 21:35:00 +0100

    activities search for employees on their dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :employees do
     resources :activities, only: [:index, :show]
-    resources :dashboards, only: [:show]
+    resources :dashboards, only: [:index]
   end
 
   namespace :trainers do
