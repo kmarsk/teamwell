de2509d2f554ab128f354e309fcdca7ca0931c74
Marta Kask <marta.m.kask@gmail.com>
2021-03-12 19:46:10 +0100

    search working, needs params

--- app/controllers/employees/activities_controller.rb
+++ app/controllers/employees/activities_controller.rb
@@ -1,9 +1,14 @@
 class Employees::ActivitiesController < ApplicationController
+
     def index
+      if params[:query].present?
+        @activities = Activity.where(category: params[:query])
+      else
         @activities = Activity.all
+      end
     end
 
     def show
         @activity= Activity.find(params[:id])
-    end    
+    end
 end
