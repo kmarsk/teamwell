0dc94556ebd379e3dd41e50ef910b7d3dbf9b106
Amanda Teixeira <amanddateixeira@outlook.com>
2021-03-17 21:28:35 +0000

    updated the trainers upcoming list

--- app/controllers/trainers/dashboards_controller.rb
+++ app/controllers/trainers/dashboards_controller.rb
@@ -1,6 +1,7 @@
 class Trainers::DashboardsController < ApplicationController
     def index
         @trainer = current_user
-        @activities = Activity.all
+        @activities = current_user.activities
+        @bookings = Booking.includes(schedule: :activity).where(activities: { id: @activities.ids}).where("schedules.date >= ?", Date.today)
     end
 end
